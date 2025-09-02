134a53e23864e2ff99a1d98dd411be92e978165a
rain12335 <105901499+rain12335@users.noreply.github.com>
2025-09-02 09:55:49 +0800

    Update gengxinfujianip.py

--- gengxinfujianip.py
+++ gengxinfujianip.py
@@ -3,7 +3,7 @@ import re
 import cv2  # 导入OpenCV库
 
 # 定义fofa链接
-fofa_url = 'https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249IkZ1amlhbiImJm9yZz0iQ2hpbmFuZXQi&page=1&page_size=50'
+fofa_url = 'https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249IkZ1amlhbiIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi'
 # fofa_url = 'http://tonkiang.us/hoteliptv.php?page=1&pv=%E5%9B%9B%E5%B7%9D%E7%94%B5%E4%BF%A1'
 
 
